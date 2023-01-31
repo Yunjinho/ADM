2a3ccab745e67e89fde12738b4dc7b824297762f
Yunjinho <dbswlsgh1238@naver.com>
2023-01-31 15:35:48 +0900

    frist commit

---
+++ ADM/src/test/java/com/adm/main/AdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adm.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
