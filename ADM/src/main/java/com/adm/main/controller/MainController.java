86ceea99caea64f9b7899de823aa389851ad0c93
hannahjjjj <wh467925@gmail.com>
2023-02-03 12:06:16 +0900

    오류확인용

--- ADM/src/main/java/com/adm/main/controller/MainController.java
+++ ADM/src/main/java/com/adm/main/controller/MainController.java
@@ -4,33 +4,26 @@ import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.adm.main.dto.ChattingroomDTO;
 import com.adm.main.repository.ChattingroomRepository;
-import com.adm.main.repository.GatherRepository;
-import com.adm.main.service.GatherService;
 
 @Controller
 public class MainController {
 	@Autowired
 	ChattingroomRepository re;
-
-	
-//	@RequestMapping("/")
-//	public String index() {
-//		ChattingroomDTO chatroom=new ChattingroomDTO().builder()
-//				.chatroomname("test")
-//				.chattime(new Date())
-//				.chatting_type(0)
-//				.build();
-//		System.out.println(chatroom);
-//		re.save(chatroom);
-//		
-//		return "NewFile";
-//	}
-	
-
+	@Autowired
+	ChattingroomDTO chatroom;
+	@RequestMapping("/index")
+	public String index() {
+		chatroom=new ChattingroomDTO().builder()
+				.chatroomname("test")
+				.chattime(new Date())
+				.chatting_type(0)
+				.build();
+//		//re.save(chatroom);		
+		System.out.println("ø÷æ»≈∏");
+		return "index";
+	}
 }
