46c32fae5ed36cd179585eb11dbd7b160da35860
hannahjjjj <wh467925@gmail.com>
2023-02-03 11:46:01 +0900

    crud

--- ADM/src/main/java/com/adm/main/controller/MainController.java
+++ ADM/src/main/java/com/adm/main/controller/MainController.java
@@ -4,26 +4,35 @@ import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.adm.main.dto.ChattingroomDTO;
 import com.adm.main.repository.ChattingroomRepository;
+import com.adm.main.repository.GatherRepository;
+import com.adm.main.service.GatherService;
 
 @Controller
 public class MainController {
 	@Autowired
 	ChattingroomRepository re;
+	GatherRepository ge;
+	GatherService gs;
 	
-	@RequestMapping("/")
-	public String index() {
-		ChattingroomDTO chatroom=new ChattingroomDTO().builder()
-				.chatroomname("test")
-				.chattime(new Date())
-				.chatting_type(0)
-				.build();
-		System.out.println(chatroom);
-		re.save(chatroom);
-		
-		return "NewFile";
-	}
+//	@RequestMapping("/")
+//	public String index() {
+//		ChattingroomDTO chatroom=new ChattingroomDTO().builder()
+//				.chatroomname("test")
+//				.chattime(new Date())
+//				.chatting_type(0)
+//				.build();
+//		System.out.println(chatroom);
+//		re.save(chatroom);
+//		
+//		return "NewFile";
+//	}
+	
+
+
 }
