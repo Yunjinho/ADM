21c19477f38d1af95f76ea2875956e60ce410eb2
Yunjinho <dbswlsgh1238@naver.com>
2023-02-06 17:01:29 +0900

    에러그만

--- ADM/src/main/java/com/adm/main/AdmApplication.java
+++ ADM/src/main/java/com/adm/main/AdmApplication.java
@@ -3,13 +3,11 @@ package com.adm.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan({"com.adm.main.dto"})
 public class AdmApplication {
 
 	public static void main(String[] args) {
