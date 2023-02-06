7b6c7a1afb87e786d572dcf037d6b40e6cd79319
hannahjjjj <wh467925@gmail.com>
2023-02-06 16:58:36 +0900

    컨트롤러 오류 해결

--- ADM/src/main/java/com/adm/main/controller/MainController.java
+++ ADM/src/main/java/com/adm/main/controller/MainController.java
@@ -1,31 +1,25 @@
 package com.adm.main.controller;
 
-import java.util.Date;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.adm.main.dto.ChattingroomDTO;
-import com.adm.main.repository.ChattingroomRepository;
+
 
 @Controller
 public class MainController {
-	@Autowired
-	ChattingroomRepository re;
-	@Autowired
-	ChattingroomDTO chatroom;
 	
 	
 	@RequestMapping("/")
-	public String index() {
-		chatroom=new ChattingroomDTO().builder()
-				.chatroomname("test")
-				.chattime(new Date())
-				.chatting_type(0)
-				.build();
-//		//re.save(chatroom);		
+	public String index() {		
 		System.out.println("¿Ö¾ÈÅ¸");
-		return "index";
+		return "main";
+	}
+	
+	@RequestMapping("/weather")
+	public String weather() {
+		System.out.println("³¯¾¾");
+		return "weather";
 	}
 }
