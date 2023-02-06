519e9056c64bc0498b98bb932efb629df376b54d
hannahjjjj <wh467925@gmail.com>
2023-02-06 17:10:26 +0900

    오류해결

--- ADM/src/main/java/com/adm/main/controller/MainController.java
+++ ADM/src/main/java/com/adm/main/controller/MainController.java
@@ -14,7 +14,7 @@ public class MainController {
 	@RequestMapping("/")
 	public String index() {		
 		System.out.println("ø÷æ»≈∏");
-		return "main";
+		return "index";
 	}
 	
 	@RequestMapping("/weather")
