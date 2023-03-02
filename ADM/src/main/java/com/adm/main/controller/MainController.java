5d3c17258e48116527e5488b0d6d5783a76779f6
Yunjinho <dbswlsgh1238@naver.com>
2023-03-02 14:59:09 +0900

    react

--- ADM/src/main/java/com/adm/main/controller/MainController.java
+++ ADM/src/main/java/com/adm/main/controller/MainController.java
@@ -2,12 +2,15 @@ package com.adm.main.controller;
 
 
 
-import org.springframework.stereotype.Controller;
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 
-@Controller
+@RestController
 public class MainController {
 	
 	
@@ -16,6 +19,11 @@ public class MainController {
 		System.out.println("왜안타");
 		return "index";
 	}
+	
+	@GetMapping("/api/hello")
+    public String hello(){
+        return "안녕하세요. 현재 서버시간은 "+new Date() +"입니다. \n";
+    }
 	
 	@RequestMapping("/weather")
 	public String weather() {
